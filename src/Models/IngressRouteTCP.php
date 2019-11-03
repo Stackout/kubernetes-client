674d19b13cef7560ead74ff38f409a3864d4ec36
Ryan Hein <me@ryanhein.com>
2019-11-03 06:30:27 -0700

    Add support for Traefik.

---
+++ src/Models/IngressRouteTCP.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Kubernetes;
+
+use Maclof\Kubernetes\Models\Model;
+
+class IngressRouteTCP extends Model
+{
+    /**
+	 * The api version.
+	 *
+	 * @var string
+	 */
+	protected $apiVersion = 'traefik.containo.us/v1alpha1';
+
+	/**
+	 * The schema.
+	 *
+	 * @var array
+	 */
+	protected $schema = [
+        'kind' => 'IngressRouteTCP'
+    ];
+}
